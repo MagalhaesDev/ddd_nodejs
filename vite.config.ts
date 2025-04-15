9deceeba64d227278ce7a420d844dcad25d814c0
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-04-15 20:28:12 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import tsConfigsPaths from 'vite-tsconfig-paths'
+
+
+export default defineConfig({
+    plugins: [tsConfigsPaths()],
+    test: {
+        globals: true
+    }
+})

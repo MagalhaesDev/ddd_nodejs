491dced9534f9582bb98021abce07e16f68ff4e8
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-05-05 18:56:17 -0300

    [feat]Get question by slug

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,7 +1,16 @@
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
-
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((question) => question.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   public items: Question[] = []
 
   async create(question: Question) {

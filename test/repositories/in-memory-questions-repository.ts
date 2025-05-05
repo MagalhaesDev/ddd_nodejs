54a49f89154270d2d3b5969aaf29c749d4643c9e
Mateus Magalhães <mateusmagalhaesemidio@gmail.com>
2025-05-05 20:47:12 -0300

    [feat]use case delete question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,6 +1,18 @@
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find(
+      (question) => question.id.toString() === id,
+    )
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((question) => question.slug.value === slug)
 
@@ -15,5 +27,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
